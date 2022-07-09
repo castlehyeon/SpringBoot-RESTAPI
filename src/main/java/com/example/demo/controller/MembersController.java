a469680ccd85de80913f334950e9033b06e405e3
castlehyeon <qnftktls118@naver.com>
2022-07-10 08:29:01 +0900

    테스트 로직 구현

--- src/main/java/com/example/demo/controller/MembersController.java
+++ src/main/java/com/example/demo/controller/MembersController.java
@@ -7,11 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//controller역할을 함을 명시, 데이터를 json으로 처리함을 알리는 RestController어노테이션
 public class MembersController {
     @Autowired
     private MembersService service;
+    //인터페이스 임포트
     @GetMapping(path = "/test")
+    //Get요청을 받아들이고, 이는 /test라는 path를 통해서 작동.
     public Member memberTest() {
         return service.testMember();
+        //테스트멤버를 반환하는 메서드
     }
 }

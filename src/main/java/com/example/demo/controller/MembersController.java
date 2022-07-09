69759d155f200674400e9122b1cfa49348860edd
castlehyeon <qnftktls118@naver.com>
2022-07-09 17:53:23 +0900

    Service작성

--- src/main/java/com/example/demo/controller/MembersController.java
+++ src/main/java/com/example/demo/controller/MembersController.java
@@ -1,16 +1,17 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.Member;
+import com.example.demo.service.MembersService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class MembersController {
+    @Autowired
+    private MembersService service;
     @GetMapping(path = "/test")
     public Member memberTest() {
-        Member testMember = new Member();
-        testMember.setId(1);
-        testMember.setName("testName");
-        testMember.setAge(15);
-        testMember.setDept("test");
-        return testMember;
+        return service.testMember();
     }
 }

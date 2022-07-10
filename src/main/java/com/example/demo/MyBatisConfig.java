1d227febae7de8f27c58a8b5532e9242ce8f1549
castlehyeon <qnftktls118@naver.com>
2022-07-10 09:06:01 +0900

    DAO 인터페이스 작성

--- src/main/java/com/example/demo/MyBatisConfig.java
+++ src/main/java/com/example/demo/MyBatisConfig.java
@@ -10,7 +10,10 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import javax.sql.DataSource;
 
     @Configuration
+    // Spring Boot 앱의 설정 역할
     @MapperScan(basePackages = "com.example.demo.dao")
+    //앞으로 작성하게 될 DAO 클래스의 패키지
+
     public class MyBatisConfig {
         @Bean
         public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
@@ -19,6 +22,7 @@ import javax.sql.DataSource;
             sessionFactory.setDataSource(dataSource);
             sessionFactory.setMapperLocations(new
                     PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
+            //소스 내부의 "classpath:mapper/*.xml"은 앞으로 SQL문을 작성할 mapper.xml 파일의 경로가 될 것
                     return sessionFactory.getObject();
 }
 }
